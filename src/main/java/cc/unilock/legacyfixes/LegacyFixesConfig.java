ef00a855cb1c0da011d5026a805608b0e8b8f6be
unilock <unilock@fennet.rentals>
2024-01-10 16:33:34 -0500

    Sort LegacyFixesConfig entries
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/legacyfixes/LegacyFixesConfig.java
+++ src/main/java/cc/unilock/legacyfixes/LegacyFixesConfig.java
@@ -8,11 +8,11 @@ public class LegacyFixesConfig {
     public static boolean bedSpawnFix = true;
     public static boolean doubleDoors = true;
     public static boolean jumpClimbing = true;
-    public static boolean slideClimbing = false;
-    public static boolean sortedEnchantments = true;
     public static boolean keepXP = false;
     public static boolean noTrample = false;
     public static boolean rmbClear = true;
+    public static boolean slideClimbing = false;
+    public static boolean sortedEnchantments = true;
 
     public static void synchronizeConfiguration(File configFile) {
         Configuration configuration = new Configuration(configFile);
@@ -20,11 +20,11 @@ public class LegacyFixesConfig {
         bedSpawnFix = configuration.getBoolean("bedSpawnFix", Configuration.CATEGORY_GENERAL, bedSpawnFix, "Allows beds to set a player's spawn point during the day (as in 1.15+)");
         doubleDoors = configuration.getBoolean("doubleDoors", Configuration.CATEGORY_GENERAL, doubleDoors, "Makes double doors open simultaneously");
         jumpClimbing = configuration.getBoolean("jumpClimbing", Configuration.CATEGORY_GENERAL, jumpClimbing, "Allows climbing ladders by jumping (incompat with slideClimbing)");
-        sortedEnchantments = configuration.getBoolean("sortedEnchantments", Configuration.CATEGORY_GENERAL, sortedEnchantments, "Sorts enchantments in item tooltips (alphabetically)");
-        slideClimbing = configuration.getBoolean("slideClimbing", Configuration.CATEGORY_GENERAL, slideClimbing, "Allows traversing ladders by looking up or down (incompat with jumpClimbing)");
         keepXP = configuration.getBoolean("keepXP", Configuration.CATEGORY_GENERAL, keepXP, "Players keep their experience level / points on death");
         noTrample = configuration.getBoolean("noTrample", Configuration.CATEGORY_GENERAL, noTrample, "Prevents trampling farmland (completely)");
         rmbClear = configuration.getBoolean("rmbClear", Configuration.CATEGORY_GENERAL, rmbClear, "Allows clearing text fields by right-clicking them");
+        slideClimbing = configuration.getBoolean("slideClimbing", Configuration.CATEGORY_GENERAL, slideClimbing, "Allows traversing ladders by looking up or down (incompat with jumpClimbing)");
+        sortedEnchantments = configuration.getBoolean("sortedEnchantments", Configuration.CATEGORY_GENERAL, sortedEnchantments, "Sorts enchantments in item tooltips (alphabetically)");
 
         if (configuration.hasChanged()) {
             configuration.save();
