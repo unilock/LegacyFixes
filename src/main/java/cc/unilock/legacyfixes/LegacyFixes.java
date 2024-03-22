4b60b02a8f9c96e9a51555d95330772384275c6b
unilock <unilock@fennet.rentals>
2024-03-22 12:17:47 -0400

    Update buildscript + start tagging versions
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/legacyfixes/LegacyFixes.java
+++ src/main/java/cc/unilock/legacyfixes/LegacyFixes.java
@@ -5,9 +5,9 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Tags.MODID, version = Tags.VERSION, name = Tags.MODNAME, acceptedMinecraftVersions = "[1.7.10]")
+@Mod(modid = "legacyfixes", version = Tags.VERSION, name = "LegacyFixes", acceptedMinecraftVersions = "[1.7.10]")
 public class LegacyFixes {
-    //public static final Logger LOGGER = LogManager.getLogger(Tags.MODID);
+    //public static final Logger LOGGER = LogManager.getLogger("LegacyFixes");
 
     @SidedProxy(clientSide = "cc.unilock.legacyfixes.proxy.ClientProxy", serverSide = "cc.unilock.legacyfixes.proxy.CommonProxy")
     public static CommonProxy proxy;
