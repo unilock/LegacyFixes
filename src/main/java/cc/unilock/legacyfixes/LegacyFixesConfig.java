55cd5af5962eb807064c5013524a8debf6f5046b
unilock <unilock@fennet.rentals>
2025-05-03 07:35:30 -0400

    mark whether configs are client- or server-side
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/legacyfixes/LegacyFixesConfig.java
+++ src/main/java/cc/unilock/legacyfixes/LegacyFixesConfig.java
@@ -5,67 +5,67 @@ import com.gtnewhorizon.gtnhlib.config.Config;
 @Config(modid = "legacyfixes")
 @Config.RequiresMcRestart
 public class LegacyFixesConfig {
-    @Config.Comment("Prevents mobs from attacking / targeting players")
+    @Config.Comment("Prevents mobs from attacking / targeting players [server]")
     @Config.DefaultBoolean(false)
     public static boolean apatheticMobs;
 
-    @Config.Comment("Allows beds to set a player's spawn point during the day (as in 1.15+)")
+    @Config.Comment("Allows beds to set a player's spawn point during the day (as in 1.15+) [server]")
     @Config.DefaultBoolean(true)
     public static boolean bedSpawnFix;
 
-    @Config.Comment("Fixes line breaks in chat not rendering properly, but breaks certain formatting in fixed chat messages")
+    @Config.Comment("Fixes line breaks in chat not rendering properly, but breaks certain formatting in fixed chat messages [client]")
     @Config.DefaultBoolean(false)
     public static boolean chatLinebreakFix;
 
-    @Config.Comment("Makes double doors open simultaneously")
+    @Config.Comment("Makes double doors open simultaneously [server]")
     @Config.DefaultBoolean(true)
     public static boolean doubleDoors;
 
-    @Config.Comment("Makes the hunger system always act as if the difficulty is set to Peaceful (incompat with AppleCore)")
+    @Config.Comment("Makes the hunger system always act as if the difficulty is set to Peaceful (incompat with AppleCore) [server]")
     @Config.DefaultBoolean(false)
     public static boolean hungerless;
 
-    @Config.Comment("Allows climbing ladders by jumping (incompat with slideClimbing)")
+    @Config.Comment("Allows climbing ladders by jumping (incompat with slideClimbing) [client]")
     @Config.DefaultBoolean(true)
     public static boolean jumpClimbing;
 
-    @Config.Comment("Players keep their experience level / points on death")
+    @Config.Comment("Players keep their experience level / points on death [server]")
     @Config.DefaultBoolean(false)
     public static boolean keepXP;
 
-    @Config.Comment("Try to fix MC-5694 (\"High efficiency tools / fast mining destroys some blocks client-side only\")")
+    @Config.Comment("Try to fix MC-5694 (\"High efficiency tools / fast mining destroys some blocks client-side only\") [server]")
     @Config.DefaultBoolean(true)
     public static boolean mc5694Fix;
 
-    @Config.Comment("Grant the \"Getting Wood\" achievement for any log, including modded")
+    @Config.Comment("Grant the \"Getting Wood\" achievement for any log, including modded [server]")
     @Config.DefaultBoolean(true)
     public static boolean mineWoodFix;
 
-    @Config.Comment("Fixes the \"NNBSP\" character in DateFormat outputs in Java 20+, as in the singleplayer world selection menu")
+    @Config.Comment("Fixes the \"NNBSP\" character in DateFormat outputs in Java 20+, as in the singleplayer world selection menu [client]")
     @Config.DefaultBoolean(true)
     public static boolean nnbspFix;
 
-    @Config.Comment("Prevents trampling farmland (completely)")
+    @Config.Comment("Prevents trampling farmland (completely) [server]")
     @Config.DefaultBoolean(false)
     public static boolean noTrample;
 
-    @Config.Comment("Allows clearing text fields by right-clicking them")
+    @Config.Comment("Allows clearing text fields by right-clicking them [client]")
     @Config.DefaultBoolean(true)
     public static boolean rmbClear;
 
-    @Config.Comment("Allows traversing ladders by looking up or down (incompat with jumpClimbing)")
+    @Config.Comment("Allows traversing ladders by looking up or down (incompat with jumpClimbing) [client]")
     @Config.DefaultBoolean(false)
     public static boolean slideClimbing;
 
-    @Config.Comment("Sorts enchantments in item tooltips (alphabetically)")
+    @Config.Comment("Sorts enchantments in item tooltips (alphabetically) [client]")
     @Config.DefaultBoolean(true)
     public static boolean sortedEnchantments;
 
-    @Config.Comment("Prevents \"Too Expensive!\" from appearing in vanilla anvils, allowing very expensive enchanting or repairing")
+    @Config.Comment("Prevents \"Too Expensive!\" from appearing in vanilla anvils, allowing very expensive enchanting or repairing [both]")
     @Config.DefaultBoolean(false)
     public static boolean tooExpensive;
 
-    @Config.Comment("Shows the harvest level (tier) of a tool in its tooltip when holding SHIFT")
+    @Config.Comment("Shows the harvest level (tier) of a tool in its tooltip when holding SHIFT [client-ish]")
     @Config.DefaultBoolean(false)
     public static boolean tooltipTier;
 }
